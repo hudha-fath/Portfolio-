7dc3c4e6a10b784e701848b3aad9afea1087e853
hudha-fath <hudha9745@gmail.com>
2026-07-13 20:39:05 +0530

    Connect frontend to Render backend

--- frontend/script.js
+++ frontend/script.js
@@ -102,7 +102,7 @@ contactForm.addEventListener("submit", async (e) => {
     };
 
     try {
-        const response = await fetch("http://localhost:5000/contact", {
+        const response = await fetch("https://portfolio-backend-jcsp.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
