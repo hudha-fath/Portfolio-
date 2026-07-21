ea09421b4a0abc8bfe1c172b8f010eb3b7897bfe
hudha-fath <hudha9745@gmail.com>
2026-07-21 21:39:35 +0530

    Removed unnecessary files

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,5 @@
 const typingElement = document.getElementById("typing");
+
 const roles = [
     "Full Stack Developer",
     "React Developer",
@@ -6,118 +7,120 @@ const roles = [
     "Python Developer",
     "Final Year B.Tech Student"
 ];
+
 let roleIndex = 0;
 let charIndex = 0;
 let deleting = false;
+
 function typeEffect() {
+
     const currentRole = roles[roleIndex];
+
     if (!deleting) {
+
         typingElement.textContent =
             currentRole.substring(0, charIndex++);
+
         if (charIndex > currentRole.length) {
             deleting = true;
             setTimeout(typeEffect, 1500);
             return;
         }
+
     } else {
+
         typingElement.textContent =
             currentRole.substring(0, charIndex--);
+
         if (charIndex < 0) {
+
             deleting = false;
+
             roleIndex++;
+
             if (roleIndex >= roles.length) {
                 roleIndex = 0;
             }
+
         }
+
     }
+
     setTimeout(typeEffect, deleting ? 60 : 120);
 }
+
 typeEffect();
+
+
 const sections = document.querySelectorAll("section");
 const navLinks = document.querySelectorAll("nav ul li a");
+
 window.addEventListener("scroll", () => {
+
     let current = "";
+
     sections.forEach(section => {
+
         const sectionTop = section.offsetTop - 120;
-        if (pageYOffset >= sectionTop) {
+
+        if (window.pageYOffset >= sectionTop) {
             current = section.getAttribute("id");
         }
+
     });
+
     navLinks.forEach(link => {
+
         link.classList.remove("active");
+
         if (link.getAttribute("href") === "#" + current) {
             link.classList.add("active");
         }
+
     });
+
 });
-const revealElements = document.querySelectorAll(
-".skill-card,.project-card,.education-card,.about-container,.hero-text,.hero-image,.contact-container"
-);
-function reveal() {
-    revealElements.forEach(el => {
-        const windowHeight = window.innerHeight;
-        const elementTop = el.getBoundingClientRect().top;
-        if (elementTop < windowHeight - 100) {
-            el.classList.add("show");
-        }
-    });
-}
-window.addEventListener("scroll", reveal);
-reveal();
+
 const topBtn = document.getElementById("topBtn");
+
 window.addEventListener("scroll", () => {
+
     if (window.scrollY > 400) {
+
         topBtn.style.display = "block";
-    }
-    else {
+
+    } else {
+
         topBtn.style.display = "none";
+
     }
+
 });
+
 topBtn.onclick = () => {
+
     window.scrollTo({
-        top:0,
-        behavior:"smooth"
+
+        top: 0,
+        behavior: "smooth"
+
     });
+
 };
 
-const navbar=document.querySelector("nav");
-window.addEventListener("scroll",()=>{
-    if(window.scrollY>50){
-        navbar.style.background="#020617";
+
+const navbar = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+
+        navbar.style.background = "#020617";
+
+    } else {
+
+        navbar.style.background = "rgba(15,23,42,.95)";
+
     }
-    else{
-     navbar.style.background="rgba(15,23,42,.95)";
-    }
-});
-const contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const data = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-    };
-
-    try {
-        const response = await fetch("https://portfolio-backend-jcsp.onrender.com", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-        });
-
-        const result = await response.json();
-
-        alert(result.message);
-
-        contactForm.reset();
-
-    } catch (error) {
-        alert("Something went wrong!");
-        console.log(error);
-    }
 });
